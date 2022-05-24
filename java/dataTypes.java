2d6765ec092abab855f9bc3b43558153cad572ae
vportela <victoria.portela03@gmail.com>
2022-05-23 21:12:18 -0400

    java day one

---
+++ java/dataTypes.java
@@ -0,0 +1,17 @@
+//everything must be inside a class
+class DataTypes { 
+
+    public static void main(String [] args){
+        System.out.println("hello world");
+    }
+    
+}
+//understand to the best of your ability what all these words in the 
+//method mean on line 4.
+//each of those words is doing something different.
+
+//figure out how to create and system out (sysout)all th emain data types
+//we learned in typescript like
+//string, number, boolean, null, undefined, array, and object.
+
+//sysout for each data type like we did above for helloworld. 

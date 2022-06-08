d3c83c9ab7f2e1de7c21c2fd16c68edb8bf21986
vportela <--global>
2022-06-08 00:37:09 -0400

    all comments user 1 recieved

--- java/socialMediaApp/SocialMediaApp.java
+++ java/socialMediaApp/SocialMediaApp.java
@@ -20,6 +20,11 @@ public class SocialMediaApp {
         return user1AllComments; 
     }
 
+    private static List<Comment> getAllCommentsOnUser1Posts(List<Comment> comments) { 
+        List<Comment> allCommentsReceivedByUser1 = comments.stream().filter(comment -> comment.getPostId() == 1).collect(Collectors.toList());
+        return allCommentsReceivedByUser1; 
+    }
+
 
     public static void main(String[] args) {
 
@@ -39,16 +44,16 @@ public class SocialMediaApp {
         Post post6 = new Post(6, "meow meow i am a catte", user3.getId());
 
         //i know there is a better way to connect the comments to an author just let me live for now. 
-        Comment comment1 = new Comment(1, "wow great post", 3, user1.getId());
-        Comment comment2 = new Comment(2, "making a comment on this post", 1, user2.getId());
-        Comment comment3 = new Comment(3, "mreowwwww", 2, user3.getId());
-        Comment comment4 = new Comment(4, "i am not a cat", 4, user1.getId());
-        Comment comment5 = new Comment(5, "i'm a little teapot", 6, user2.getId());
-        Comment comment6 = new Comment(6, "boop boop beep beep", 1, user2.getId());
-        Comment comment7 = new Comment(7, "meow meow", 6, user1.getId());
-        Comment comment8 = new Comment(8, "brb dyin", 2, user3.getId());
-        Comment comment9 = new Comment(9, "this sucks", 4, user3.getId());
-        Comment comment10 = new Comment(10, "cat nap", 3, user3.getId());
+        Comment comment1 = new Comment(1, "wow great post", post3.getId(), user1.getId());
+        Comment comment2 = new Comment(2, "making a comment on this post", post1.getId(), user2.getId());
+        Comment comment3 = new Comment(3, "mreowwwww", post2.getId(), user3.getId());
+        Comment comment4 = new Comment(4, "i am not a cat", post4.getId(), user1.getId());
+        Comment comment5 = new Comment(5, "i'm a little teapot", post6.getId(), user2.getId());
+        Comment comment6 = new Comment(6, "boop boop beep beep", post1.getId(), user2.getId());
+        Comment comment7 = new Comment(7, "meow meow", post6.getId(), user1.getId());
+        Comment comment8 = new Comment(8, "brb dyin", post2.getId(), user3.getId());
+        Comment comment9 = new Comment(9, "this sucks", post4.getId(), user3.getId());
+        Comment comment10 = new Comment(10, "cat nap", post3.getId(), user3.getId());
 
 //if comment.postId == 1 && post.userId == 1 then the user who 
         List<Post> posts = List.of(post1,post2,post3,post4,post5,post6);
@@ -64,10 +69,12 @@ public class SocialMediaApp {
         Integer user1Posts = getUser1Posts(posts);
         List<Comment> user1PostComments = getCommentsOnUser1Posts(comments);
         List<Comment> user1AllComments = getAllUser1Comments(comments);
+        List<Comment> allCommentsReceivedByUser1 = getAllCommentsOnUser1Posts(comments);
         System.out.println("---------User 1---------");
         System.out.println("Posts: " + user1Posts);
         System.out.println("User1 post 1 has 2 comments: " + user1PostComments);
         System.out.println("All comments made by User1: " + user1AllComments);
+        System.out.println("All comments received by User1: " + allCommentsReceivedByUser1);
 
         
     }

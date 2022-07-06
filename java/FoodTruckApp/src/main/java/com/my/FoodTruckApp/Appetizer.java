c409a20d29612fcea0cc3442c98002f90edc3669
vportela <--global>
2022-07-06 19:16:51 -0400

    finished set up

---
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/Appetizer.java
@@ -0,0 +1,18 @@
+package com.my.FoodTruckApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Appetizer {
+
+    private Integer id;
+    private String title;
+    private String description;
+    private Integer price;
+
+}
---
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerController.java
@@ -0,0 +1,48 @@
+package com.my.FoodTruckApp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class AppetizerController {
+
+    private  final Appetizer appetizer1 = new Appetizer(1,"chicken skewer", "yummy chicken on a stick", 4);
+    private  final Appetizer appetizer2 = new Appetizer(2,"pork taco", "Delicious little pocket of pork joy", 8);
+    ArrayList<Appetizer> appetizers = new ArrayList<>(Arrays.asList(appetizer1,appetizer2));
+
+    @GetMapping("/menu")
+        public List<Appetizer> getAppetizers() {
+        System.out.println("getting the appetizer menu" + appetizers);
+
+        return appetizers;
+    }
+
+
+    @PostMapping("/menu")
+    public Appetizer createItem(@RequestBody Appetizer requestBody) {
+        System.out.println("creating an appetizer with requestBody: " + requestBody);
+
+        return requestBody;
+    }
+
+    @GetMapping("/menu/{id}")
+    public Appetizer getAppetizerById(@PathVariable Integer id) {
+        System.out.println("Getting appetizer by id: " + id);
+
+        Optional<Appetizer> appetizerById = appetizers.stream().filter(appetizer -> appetizer.getId().equals(id)).findFirst();
+
+        return appetizerById.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("menu/{id}")
+    Appetizer changePrice(@RequestBody Appetizer requestBody, @PathVariable Integer id) {
+
+    }
+
+}

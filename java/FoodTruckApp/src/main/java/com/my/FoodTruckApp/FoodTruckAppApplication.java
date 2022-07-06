8c40f5c4df3a386e9859280d43ada36b6df9f4f6
vportela <--global>
2022-07-06 19:58:56 -0400

    put request is almost working but going to try something else to see if it works better

---
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/FoodTruckAppApplication.java
@@ -0,0 +1,19 @@
+package com.my.FoodTruckApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTruckAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTruckAppApplication.class, args);
+
+		System.out.println("this is a test");
+
+		Appetizer myAppetizer = new Appetizer(1, "chicken", "tasty chicken for eating", 4);
+		System.out.println("myAppetizer" + myAppetizer);
+	}
+
+
+}
---
+++ java/FoodTruckApp/src/test/java/com/my/FoodTruckApp/FoodTruckAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.FoodTruckApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

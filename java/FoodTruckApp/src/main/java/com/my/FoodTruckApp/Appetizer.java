bf8a104302c1c40bfd113e403b061c8fa4885bef
vportela <--global>
2022-07-06 21:43:21 -0400

    put request added

--- java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/Appetizer.java
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/Appetizer.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
 public class Appetizer {
 
     private Integer id;
--- java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerController.java
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerController.java
@@ -14,7 +14,9 @@ public class AppetizerController {
 
     private  final Appetizer appetizer1 = new Appetizer(1,"chicken skewer", "yummy chicken on a stick", 4);
     private  final Appetizer appetizer2 = new Appetizer(2,"pork taco", "Delicious little pocket of pork joy", 8);
-    ArrayList<Appetizer> appetizers = new ArrayList<>(Arrays.asList(appetizer1,appetizer2));
+
+    private  final Appetizer appetizer3 = new Appetizer(3,"pork taco", "Delicious little pocket of pork joy", 8);
+    ArrayList<Appetizer> appetizers = new ArrayList<>(Arrays.asList(appetizer1,appetizer2, appetizer3));
 
     @GetMapping("/menu")
         public List<Appetizer> getAppetizers() {
@@ -22,11 +24,21 @@ public class AppetizerController {
 
         return appetizers;
     }
-
-
+// ToDo: change put to allow you to change all fields except for id.
+    // ToDo: create a patch that lets you change a specific field.
+    //ToDo: make sure that your URLS make more sense with your model/class.
+    //ToDo: make sure your POST api is actually adding your new items to the list of objects.
+    //ToDo: PUT should require you to update the entire object, not just a field.
+    //RESTful API, have conventions or patterns.
+    //they should return JSON, and recieve JSON
+    //use HTTP methods
+    // urls should always be plural and they should be about the model you are dealing with
+    //so the URL here should be appetizers. It needs to be self-explanatory and related to the model/class.
     @PostMapping("/menu")
     public Appetizer createItem(@RequestBody Appetizer requestBody) {
         System.out.println("creating an appetizer with requestBody: " + requestBody);
+        //add this new appetizer to the appetizers ArrayList<Appetizer>
+
 
         return requestBody;
     }
@@ -47,11 +59,15 @@ public class AppetizerController {
     @PutMapping("menu/{id}")
     public Appetizer changePrice(@RequestBody Appetizer requestBody, @PathVariable Integer id) {
 
-//        Optional<Appetizer> appetizerById = appetizers.stream().filter(appetizer -> appetizer.getId().equals(id)).findFirst();
-        Appetizer appetizerDetails = appetizers.get(id);
-        appetizerDetails.setPrice(requestBody.getPrice());
+        Optional<Appetizer> optionalAppetizerById = appetizers.stream().filter(appetizer -> appetizer.getId().equals(id)).findFirst();
 
-        return appetizerDetails;
+        if (optionalAppetizerById.isPresent()) {
+            Appetizer foundAppetizer = optionalAppetizerById.get();
+            foundAppetizer.setPrice(requestBody.getPrice());
+            // trying to return the found appetizer with the updated price
+            return foundAppetizer;
+        } //if the Optional does not exist, throw the error code not found.
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
     }
 

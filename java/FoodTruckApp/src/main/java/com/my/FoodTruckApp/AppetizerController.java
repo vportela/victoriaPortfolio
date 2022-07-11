9c739a0d95f0684bd8f1207e6edf2a58223c1081
vportela <--global>
2022-07-11 18:27:39 -0400

    added dependency injection

--- java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerController.java
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerController.java
@@ -15,17 +15,18 @@ import java.util.Optional;
 @RestController
 public class AppetizerController {
 
-    private  final Appetizer appetizer1 = new Appetizer(1,"chicken skewer", "yummy chicken on a stick", 4);
-    private  final Appetizer appetizer2 = new Appetizer(2,"pork taco", "Delicious little pocket of pork joy", 8);
+    @Autowired
+    private AppetizerService appetizerService;
 
-    private  final Appetizer appetizer3 = new Appetizer(3,"mango pie", "hot mango pie", 70);
-    ArrayList<Appetizer> appetizers = new ArrayList<>(Arrays.asList(appetizer1,appetizer2, appetizer3));
+    @GetMapping("/dependencyInjection")
+    public String testingDependencyInjection() {
+
+        return appetizerService.testingService();
+    }
 
     @GetMapping("/appetizers")
         public List<Appetizer> getAppetizers() {
-        System.out.println("getting the appetizer menu" + appetizers);
-
-        return appetizers;
+        return appetizerService.getAppetizers();
     }
 
     //RESTful API, have conventions or patterns.
@@ -35,105 +36,29 @@ public class AppetizerController {
     //so the URL here should be appetizers. It needs to be self-explanatory and related to the model/class.
     @PostMapping("/appetizers")
     public Appetizer createItem(@RequestBody Appetizer requestBody) {
-        System.out.println("creating an appetizer with requestBody: " + requestBody);
-        //add this new appetizer to the appetizers ArrayList<Appetizer>
-        Integer id = appetizers.get(appetizers.size() - 1).getId() + 1;
-
-        Appetizer appetizer = new Appetizer(
-                id,
-                requestBody.getTitle(),
-                requestBody.getDescription(),
-                requestBody.getPrice()
-        );
-        appetizers.add(appetizer);
-        return appetizer;
+        return appetizerService.createItem(requestBody);
     }
 
     @GetMapping("/appetizers/{id}")
     public Appetizer getAppetizerById(@PathVariable Integer id) {
-        System.out.println("Getting appetizer by id: " + id);
 
-        Optional<Appetizer> appetizerById = appetizers.stream().filter(appetizer -> appetizer.getId().equals(id)).findFirst();
 
-        return appetizerById.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return appetizerService.getAppetizerById(id);
     }
 
-    //locate the post by the id
-    //remove the post with the matching id
-    //add
-
+//    //locate the post by the id
+//    //remove the post with the matching id
+//    //add
+//
     @PutMapping("appetizers/{id}")
     public Appetizer changeAppetizer(@RequestBody Appetizer requestBody, @PathVariable Integer id) {
 
-        Optional<Appetizer> optionalAppetizerById = appetizers.stream().filter(appetizer -> appetizer.getId().equals(id)).findFirst();
-
-//        ValidationUtils.rejectIfEmpty();
-        if (optionalAppetizerById.isPresent()) {
-            Appetizer foundAppetizer = optionalAppetizerById.get();
-            foundAppetizer.setPrice(requestBody.getPrice());
-            foundAppetizer.setDescription(requestBody.getDescription());
-            foundAppetizer.setTitle(requestBody.getTitle());
-
-            if(requestBody.getPrice() == null) {
-                System.out.println("price is null");
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-            } if (requestBody.getDescription() == null) {
-                System.out.println("description is null");
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-            } if (requestBody.getTitle() == null) {
-                System.out.println("title is null");
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-            }
-
-            // trying to return the found appetizer with all of the allowed fields updated
-            return foundAppetizer;
-        } //if the Optional does not exist, throw the error code not found.
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-        //if a field is missing from the new data, throw an exception.
-        //how do you know if a field is missing? -- it is null
-
-        //possible option, if you have a "safe appetizer" type of thing that only gives access to the fields you
-        //want to be able to change, and then if the arguments don't match, you throw the exception.
-
-
+        return appetizerService.changeAppetizer(requestBody, id);
     }
 
     @PatchMapping("appetizers/{id}")
     public Appetizer changePrice(@RequestBody Appetizer requestBody, @PathVariable Integer id) {
-        //find app by it's id
-        Optional<Appetizer> optionalAppetizerById = appetizers.stream().filter(appetizer -> appetizer.getId().equals(id)).findFirst();
-        //change optional appetizer into just Appetizer so you can set things.
-        if (optionalAppetizerById.isPresent()) {
-            Appetizer foundAppetizer = optionalAppetizerById.get();
-
-            // trying to return the found appetizer with the updated price
-
-            if(requestBody.getPrice() == null) {
-                System.out.println("found app before set price" + foundAppetizer);
-                return foundAppetizer;
-            } foundAppetizer.setPrice(requestBody.getPrice());
-            System.out.println("found app after set price" + foundAppetizer);
-
-            //this way doesnt work because if this if statement returns, it stops.
-            // if it runs the else (aka makes the change), it continues to the next if statement because
-            //there was technically no return, but then it will stop once that one returns something.
-
-            if(requestBody.getTitle() == null) {
-                System.out.println("title before change" + foundAppetizer);
-                return foundAppetizer;
-            } foundAppetizer.setTitle(requestBody.getTitle());
-            System.out.println("title after change" + foundAppetizer);
-
-//            if(requestBody.getDescription() == null) {
-//                System.out.println("title before change" + foundAppetizer);
-//                return foundAppetizer;
-//            } foundAppetizer.setDescription(requestBody.getDescription());
-//            System.out.println("title after change" + foundAppetizer);
-
-            return foundAppetizer;
-        }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+       return appetizerService.changePrice(requestBody,id);
     }
 
 }
--- java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerService.java
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerService.java
@@ -1,11 +1,148 @@
 package com.my.FoodTruckApp;
 
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Service
+@Data
 public class AppetizerService {
 
-    public void getAppetizers() {
+    public String testingService() {
 
+        System.out.println("this is the AppetizerService, hewwo");
+        String testing = "testing the app service";
+        return testing;
+    }
+
+    private  final Appetizer appetizer1 = new Appetizer(1,"chicken skewer", "yummy chicken on a stick", 4);
+    private  final Appetizer appetizer2 = new Appetizer(2,"pork taco", "Delicious little pocket of pork joy", 8);
+
+    private  final Appetizer appetizer3 = new Appetizer(3,"mango pie", "hot mango pie", 70);
+    ArrayList<Appetizer> appetizers = new ArrayList<>(Arrays.asList(appetizer1,appetizer2, appetizer3));
+
+    //------------------------GETTING LIST OF APPS-------------------------
+
+    public List<Appetizer> getAppetizers() {
+        System.out.println("getting the appetizer menu" + appetizers);
+
+        return appetizers;
+    }
+
+    // -------------------------------CREATE APP----------------------------
+
+    @PostMapping("/appetizers")
+    public Appetizer createItem(@RequestBody @NotNull Appetizer requestBody) {
+        System.out.println("creating an appetizer with requestBody: " + requestBody);
+        //add this new appetizer to the appetizers ArrayList<Appetizer>
+        Integer id = appetizers.get(appetizers.size() - 1).getId() + 1;
+
+        Appetizer appetizer = new Appetizer(
+                id,
+                requestBody.getTitle(),
+                requestBody.getDescription(),
+                requestBody.getPrice()
+        );
+        appetizers.add(appetizer);
+        return appetizer;
+    }
+
+    //-------------GET APPS BY ID --------------
+
+    @GetMapping("/appetizers/{id}")
+    public Appetizer getAppetizerById(@PathVariable Integer id) {
+        System.out.println("Getting appetizer by id: " + id);
+
+        Optional<Appetizer> appetizerById = appetizers.stream().filter(appetizer -> appetizer.getId().equals(id)).findFirst();
+
+        return appetizerById.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    // --------------------CHANGING/UPDATING AN APP BY ID ------------
+
+
+    @PutMapping("appetizers/{id}")
+    public Appetizer changeAppetizer(@RequestBody Appetizer requestBody, @PathVariable Integer id) {
+
+        Optional<Appetizer> optionalAppetizerById = appetizers.stream().filter(appetizer -> appetizer.getId().equals(id)).findFirst();
+
+//        ValidationUtils.rejectIfEmpty();
+        if (optionalAppetizerById.isPresent()) {
+            Appetizer foundAppetizer = optionalAppetizerById.get();
+            foundAppetizer.setPrice(requestBody.getPrice());
+            foundAppetizer.setDescription(requestBody.getDescription());
+            foundAppetizer.setTitle(requestBody.getTitle());
+
+            if(requestBody.getPrice() == null) {
+                System.out.println("price is null");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            } if (requestBody.getDescription() == null) {
+                System.out.println("description is null");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            } if (requestBody.getTitle() == null) {
+                System.out.println("title is null");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+//
+//            // trying to return the found appetizer with all of the allowed fields updated
+            return foundAppetizer;
+        }
+//      if the Optional does not exist, throw the error code not found.
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+            //if a field is missing from the new data, throw an exception.
+            //how do you know if a field is missing? -- it is null
+
+            //possible option, if you have a "safe appetizer" type of thing that only gives access to the fields you
+            //want to be able to change, and then if the arguments don't match, you throw the exception.
+
+    }
+
+    //-------------------- PATCHING ---------------------
+
+
+    @PatchMapping("appetizers/{id}")
+    public Appetizer changePrice(@RequestBody Appetizer requestBody, @PathVariable Integer id) {
+        //find app by it's id
+        Optional<Appetizer> optionalAppetizerById = appetizers.stream().filter(appetizer -> appetizer.getId().equals(id)).findFirst();
+        //change optional appetizer into just Appetizer so you can set things.
+        if (optionalAppetizerById.isPresent()) {
+            Appetizer foundAppetizer = optionalAppetizerById.get();
+
+            // trying to return the found appetizer with the updated price
+
+            if(requestBody.getPrice() == null) {
+                System.out.println("found app before set price" + foundAppetizer);
+                return foundAppetizer;
+            } foundAppetizer.setPrice(requestBody.getPrice());
+            System.out.println("found app after set price" + foundAppetizer);
+
+            //this way doesnt work because if this if statement returns, it stops.
+            // if it runs the else (aka makes the change), it continues to the next if statement because
+            //there was technically no return, but then it will stop once that one returns something.
+
+            if(requestBody.getTitle() == null) {
+                System.out.println("title before change" + foundAppetizer);
+                return foundAppetizer;
+            } foundAppetizer.setTitle(requestBody.getTitle());
+            System.out.println("title after change" + foundAppetizer);
+
+//            if(requestBody.getDescription() == null) {
+//                System.out.println("title before change" + foundAppetizer);
+//                return foundAppetizer;
+//            } foundAppetizer.setDescription(requestBody.getDescription());
+//            System.out.println("title after change" + foundAppetizer);
+
+            return foundAppetizer;
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }

6bff4f17f22e9a66188b835fb37b7e850cb2bfbb
vportela <--global>
2022-07-11 15:38:09 -0400

    patch updates price, if price is not updated it will return the previous price instead of null

--- java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerController.java
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerController.java
@@ -1,5 +1,6 @@
 package com.my.FoodTruckApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
---
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/AppetizerService.java
@@ -0,0 +1,11 @@
+package com.my.FoodTruckApp;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppetizerService {
+
+    public void getAppetizers() {
+
+    }
+}

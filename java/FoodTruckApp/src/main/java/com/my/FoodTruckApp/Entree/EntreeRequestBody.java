537d92b6fec79044f22383e04c76d5bae5d8cebb
vportela <--global>
2022-07-12 19:32:24 -0400

    entree lets goooooo

--- java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/Entree/EntreeRequestBody.java
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/Entree/EntreeRequestBody.java
@@ -10,6 +10,5 @@ import lombok.NoArgsConstructor;
 public class EntreeRequestBody {
     private String entreeSize;
     private String entreeFlavor;
-    private String entreePairedMeal;
     private Integer entreePrice;
 }

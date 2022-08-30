2e197d13790268b265df61dcf5de98cdb82cfeab
vp <vp@tsl.io>
2022-08-29 21:14:33 -0400

    changing log

--- java/FoodTruckApp/src/main/java/com/my/foodTruckApp/customer/CustomerRepository.java
+++ java/FoodTruckApp/src/main/java/com/my/foodTruckApp/customer/CustomerRepository.java
@@ -2,12 +2,14 @@ package com.my.foodTruckApp.customer;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerRepository {
 
     private final JdbcTemplate jdbcTemplate;
@@ -18,7 +20,7 @@ public class CustomerRepository {
         String sql = "INSERT INTO customer(first_name,last_name) VALUES(?, ?)";
         Integer rows = jdbcTemplate.update(sql, customerRequestBody.getCustomerFirstName(), customerRequestBody.getCustomerLastName());
         if(rows > 0) {
-            System.out.println("A new customer has been inserted (REQUEST BODY)");
+            log.info("A new customer has been inserted (REQUEST BODY)");
         }
         return "CREATING A CUSTOMER WORKED";
     }

383d4df78cfc7806c33784535549a4c8a8820ce3
vp <vp@tsl.io>
2022-08-29 20:20:33 -0400

    created customer repository

--- java/FoodTruckApp/src/main/java/com/my/foodTruckApp/customer/CustomerRepository.java
+++ java/FoodTruckApp/src/main/java/com/my/foodTruckApp/customer/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.my.foodTruckApp.customer;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -7,16 +8,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
-public class CustomerService {
+public class CustomerRepository {
 
     private final JdbcTemplate jdbcTemplate;
-    private final CustomerRepository customerRepository;
 
     //-------- create new customer ---------
 
     public String createNewCustomer(@RequestBody CustomerRequestBody customerRequestBody) {
-
-        return customerRepository.createNewCustomer(customerRequestBody);
+        String sql = "INSERT INTO customer(first_name,last_name) VALUES(?, ?)";
+        Integer rows = jdbcTemplate.update(sql, customerRequestBody.getCustomerFirstName(), customerRequestBody.getCustomerLastName());
+        if(rows > 0) {
+            System.out.println("A new customer has been inserted (REQUEST BODY)");
+        }
+        return "CREATING A CUSTOMER WORKED";
     }
 
 }
